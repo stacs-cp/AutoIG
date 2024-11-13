1ab324b901fe84ca6ee5c2f6648ef92da5b9a680
vincepick <vap2@st-andrews.ac.uk>
2024-11-13 15:36:03 +0000

    feat: finding minion and SR paths as well

--- bin/testIndividualPath.sh
+++ bin/testIndividualPath.sh
@@ -3,11 +3,25 @@
 # this is a script that will never be used in deployment, is just for Vincent to use during development while trying to get the container working
 
 # define test path
-TEST_PATH="/root/.local/bin/conjure"
+# TEST_PATH="/root/.local/bin/conjure"
 
 # checking if the file exists at the path
-if [ -f "$TEST_PATH" ]; then
-    echo "Conjure path added to PATH."
+if [ -f "/root/.local/bin/conjure" ]; then
+    echo "Conjure path found"
 else
     echo "Error: Conjure path '$TEST_PATH' does not exist."
 fi
+
+if [ -f "/root/.local/bin/minion" ]; then
+    echo "Minion path found"
+else
+    echo "Error: Minion path '$TEST_PATH' does not exist."
+fi
+
+if [ -f "/root/.local/bin/savilerow" ]; then
+    echo "SR path found"
+else
+    echo "Error: SR path '$TEST_PATH' does not exist."
+fi
+
+# savilerow
