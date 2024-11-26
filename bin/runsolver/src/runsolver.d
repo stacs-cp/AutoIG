bfc296302c941e4f131413d1833e3f28ffdfce72
Tudor Huica <tudor.huica@gmail.com>
2024-11-26 12:43:59 +0000

    deleted heavy files for pr

--- bin/runsolver/src/runsolver.d
+++
@@ -1,3 +0,0 @@
-runsolver.d runsolver.o: runsolver.cc Cores.hh SignalNames.hh ProcessList.hh \
- CircularBufferFilter.hh ExecutionSummary.hh Watcher.hh ProcessTree.hh \
- ProcessData.hh ProcessHistory.hh TimeStamper.hh
