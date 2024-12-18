4232d718374255b21a01e91a93c4a6c5e3731c91
vincepick <vap2@st-andrews.ac.uk>
2024-12-18 20:39:39 +0000

    build: update the uipdate-orpath to use working path

--- bin/update-or-path.sh
+++ bin/update-or-path.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+BIN_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 # Location of file to update:
-FILE="/AutoIG/bin/minizinc/share/minizinc/solvers/cpsat.msc"
+FILE="$BIN_DIR/minizinc/share/minizinc/solvers/cpsat.msc"
 
 #update name field to ortools
 sed -i 's/"name": "OR Tools CP-SAT"/"name": "ortools"/' "$FILE"
