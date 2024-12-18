90d6369b839d026d436b1efd70c6d7dd843b8447
vincepick <vap2@st-andrews.ac.uk>
2024-12-18 18:08:19 +0000

    build: making container-set-path use existing redundant installations again

--- bin/container-set-path.sh
+++ bin/container-set-path.sh
@@ -29,26 +29,17 @@ export PATH=$BIN_DIR/irace/bin:$PATH
 export R_LIBS=$BIN_DIR/:$R_LIBS
 
 
-# minizinc
-# export PATH=$BIN_DIR/minizinc/bin/:$PATH
-# #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$BIN_DIR/minizinc/lib
-# export MZN_SOLVER_PATH=$BIN_DIR/minizinc/share/minizinc/solvers
-# export MZN_STDLIB_DIR=$BIN_DIR/minizinc/share/minizinc/
-
-# minizinc from container
-export PATH=$CONTAINER_BIN_DIR/share/minizinc/:$PATH
-export MZN_SOLVER_PATH=$CONTAINER_BIN_DIR/share/minizinc/solvers
-export MZN_STDLIB_DIR=$CONTAINER_BIN_DIR/share/minizinc/
+# minizinc, use version in AutoIG 
+export PATH=$BIN_DIR/minizinc/bin/:$PATH
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$BIN_DIR/minizinc/lib
+export MZN_SOLVER_PATH=$BIN_DIR/minizinc/share/minizinc/solvers
+export MZN_STDLIB_DIR=$BIN_DIR/minizinc/share/minizinc/
 
 
-# ortools - installed as fzn-cp-sat in the Conjure installation. 
-
-export PATH=$CONTAINER_BIN_DIR/fzn-cp-sat/:$PATH
-
-
-# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$BIN_DIR/ortools/lib
-# This installation could be wrong, needs further testing as there are numerous options
-export LD_LIBRARY_PATH=$CONTAINER_BIN_DIR/lib/libortools.so.9.8.9999:$LD_LIBRARY_PATH
+# Using redundant installations
+# ortools
+export PATH=$BIN_DIR/ortools/bin/:$PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$BIN_DIR/ortools/lib
 
 # yuck
 export PATH=$BIN_DIR/yuck/bin/:$PATH
@@ -59,5 +50,24 @@ export PATH=$BIN_DIR/picat/:$PATH
 # runsolver
 export PATH=$BIN_DIR/runsolver/src/:$PATH
 
-# Not currently used
-# $BIN_DIR"/setMznSolvers.sh" 
+
+# ortools - installed as fzn-cp-sat in the Conjure installation. 
+
+# export PATH=$CONTAINER_BIN_DIR/fzn-cp-sat/:$PATH
+
+
+# # export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$BIN_DIR/ortools/lib
+# # This installation could be wrong, needs further testing as there are numerous options
+# export LD_LIBRARY_PATH=$CONTAINER_BIN_DIR/lib/libortools.so.9.8.9999:$LD_LIBRARY_PATH
+
+# # yuck
+# export PATH=$BIN_DIR/yuck/bin/:$PATH
+
+# # picat
+# export PATH=$BIN_DIR/picat/:$PATH
+
+# # runsolver
+# export PATH=$BIN_DIR/runsolver/src/:$PATH
+
+# # Not currently used
+# # $BIN_DIR"/setMznSolvers.sh" 
