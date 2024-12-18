24a3e662dfe550c0bcb1282dc7412033dad6fd36
vincepick <vap2@st-andrews.ac.uk>
2024-12-18 20:44:22 +0000

    fix: updated update script again

--- bin/update-or-path.sh
+++ bin/update-or-path.sh
@@ -3,7 +3,7 @@
 BIN_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 # Location of file to update:
 FILE="$BIN_DIR/minizinc/share/minizinc/solvers/cpsat.msc"
-
+echo $FILE
 #update name field to ortools
 sed -i 's/"name": "OR Tools CP-SAT"/"name": "ortools"/' "$FILE"
 # Verify  change
