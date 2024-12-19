ceea7de7e2eb866a7a2f148293be59c0e4c867cc
vincepick <vap2@st-andrews.ac.uk>
2024-12-19 14:03:05 +0000

    test: updated checker for chuffed with Conjure

--- scripts/testScripts/conjure_solve-tests/check_chuffed.sh
+++ scripts/testScripts/conjure_solve-tests/check_chuffed.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DATA_DIR=../essence_testing_data
 ls $DATA_DIR
-conjure solve model.essence test.param --solver=chuffed
+conjure solve $DATA_DIR/model.essence $DATA_DIR/test.param --solver=chuffed
