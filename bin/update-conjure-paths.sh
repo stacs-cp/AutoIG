63a7e2ca7ef9d572fe54bfbd33f581d62687e782
vincepick <vap2@st-andrews.ac.uk>
2024-12-19 00:55:02 +0000

    fix: updated update conjure path script again

--- bin/update-conjure-paths.sh
+++ bin/update-conjure-paths.sh
@@ -2,10 +2,10 @@
 BIN_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
 # Remove chuffed from container, and use one in MZN bin
-rm "~/.local/bin/fzn-chuffed"
+rm ~/.local/bin/fzn-chuffed
 
 # Remove minizinc from container, use mzn one
-rm "~/.local/bin/minizinc"
+rm ~/.local/bin/minizinc
 
 export PATH="/AutoIG/bin/minizinc/bin:$PATH"
 
