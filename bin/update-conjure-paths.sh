0a73a293eb24711ab9f2b18b5cab0cc73783ae58
vincepick <vap2@st-andrews.ac.uk>
2024-12-19 00:41:29 +0000

    build: added new script to update conjures paths

---
+++ bin/update-conjure-paths.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+BIN_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+# Remove chuffed from container, and use one in MZN bin
+rm "~/.local/bin/fzn-chuffed"
+
+# Remove minizinc from container, use mzn one
+rm "~/.local/bin/minizinc"
+
+export PATH="/AutoIG/bin/minizinc/bin:$PATH"
+
+echo "Paths updated for Conjure"
