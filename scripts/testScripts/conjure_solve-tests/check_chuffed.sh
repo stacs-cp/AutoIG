240720c46d76bb47c8799f6cc326edd9cc36beb2
vincepick <vap2@st-andrews.ac.uk>
2024-12-19 14:38:41 +0000

    test: update test scripts again

--- scripts/testScripts/conjure_solve-tests/check_chuffed.sh
+++ scripts/testScripts/conjure_solve-tests/check_chuffed.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-DATA_DIR=/essence_testing_data
+DATA_DIR=essence_testing_data
 ls $DATA_DIR
 conjure solve $DATA_DIR/model.essence $DATA_DIR/test.param --solver=chuffed
+rm -r conjure_solve-tests/conjure-output
--- scripts/testScripts/conjure_solve-tests/or-tools.sh
+++ scripts/testScripts/conjure_solve-tests/or-tools.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-DATA_DIR=/essence_testing_data
+DATA_DIR=essence_testing_data
 conjure solve $DATA_DIR/model.essence $DATA_DIR/test.param --solver=or-tools
+rm -r conjure_solve-tests/conjure-output
