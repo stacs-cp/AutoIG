1a2e3255fdbfc54a4aeb48f7ec23a53b8d975e03
vincepick <vap2@st-andrews.ac.uk>
2024-12-22 09:04:36 +0000

    fix: deleted testscripts in this branch, should be in the test/maccGradedCase branch instead

--- scripts/testScripts/conjure_solve-tests/kissat.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-DATA_DIR=essence_testing_data
-ls
-ls $DATA_DIR
-conjure solve $DATA_DIR/model.essence $DATA_DIR/test.param --solver=kissat
-rm -r conjure_solve-tests/conjure-output
--- scripts/testScripts/pr_discrim_tests/chuffed-maccDiscriminatingGen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo $AUTOIG
-mkdir -p "$AUTOIG/experiments/macc-discrim-gen"
-cd "$AUTOIG/experiments/macc-discrim-gen"
-python $AUTOIG/scripts/setup.py --generatorModel $AUTOIG/data/models/macc/generator.essence --problemModel $AUTOIG/data/models/macc/problem.mzn --instanceSetting discriminating --minSolverTime 1 --maxSolverTime 3 --baseSolver chuffed --solverFlags="-f" --favouredSolver ortools --favouredSolverFlags="-f" --maxEvaluations 180 --genSolverTimeLimit 5
-bash "$AUTOIG/experiments/macc-discrim-gen/run.sh"
