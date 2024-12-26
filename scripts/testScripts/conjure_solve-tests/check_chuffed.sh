6440fbd2eea7fb9a80bde04220fe52e4d90c99b8
vincepick <vap2@st-andrews.ac.uk>
2024-12-26 13:03:09 -0800

    fix: updated path to delete

--- scripts/testScripts/conjure_solve-tests/check_chuffed.sh
+++ scripts/testScripts/conjure_solve-tests/check_chuffed.sh
@@ -5,4 +5,4 @@ DATA_DIR=essence_testing_data
 ls
 ls $DATA_DIR
 conjure solve $DATA_DIR/model.essence $DATA_DIR/test.param --solver=chuffed
-rm -r conjure_solve-tests/conjure-output
+rm -r conjure-output
--- scripts/testScripts/conjure_solve-tests/or-tools.sh
+++ scripts/testScripts/conjure_solve-tests/or-tools.sh
@@ -4,4 +4,4 @@
 DATA_DIR=essence_testing_data
 ls
 conjure solve $DATA_DIR/model.essence $DATA_DIR/test.param --solver=or-tools
-rm -r conjure_solve-tests/conjure-output
+rm -r conjure-output
