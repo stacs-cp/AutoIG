33458f2591501f1cdf1e2a959c3481a8a0ac192e
vincepick <vap2@st-andrews.ac.uk>
2025-05-07 18:11:20 +0000

    fix: update scripts for testing workflow

--- scripts/testScripts/pr_discrim_tests/chuffed-vesselDiscriminatingGen.sh
+++ scripts/testScripts/pr_discrim_tests/chuffed-vesselDiscriminatingGen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Testing graded instance generation for the Macc problem using its small generator with the chuffed solver"
-mkdir -p "$AUTOIG/experiments/vessel_loading"
-cd "$AUTOIG/experiments/vessel_loading"
+# Testing graded instance generation for the Vessel Loading problem using its small generator with the chuffed solver"
+mkdir -p "$AUTOIG/experiments/vessel_loading_discrim"
+cd "$AUTOIG/experiments/vessel_loading_discrim"
 python3 "$AUTOIG/scripts/setup.py" --generatorModel "$AUTOIG/data/models/vessel-loading/generator.essence" --problemModel "$AUTOIG/data/models/vessel-loading/problem.essence" --instanceSetting discriminating --minSolverTime 1 --maxSolverTime 3 --baseSolver chuffed --solverFlags="-f" --favouredSolver ortools --favouredSolverFlags="-f" --maxEvaluations 180 --genSolverTimeLimit 5
-bash "$AUTOIG/experiments/vessel_loading/run.sh"
+bash "$AUTOIG/experiments/vessel_loading_discrim/run.sh"
