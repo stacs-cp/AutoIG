0b2157194f6bf450c2a686e773cfc3d4911a43a4
vincepick <vap2@st-andrews.ac.uk>
2025-05-07 20:55:56 +0000

    fix: update test flow, lowering upper bounds so will run faster

--- scripts/testScripts/pr_discrim_tests/chuffed-maccDiscriminatingGen.sh
+++ scripts/testScripts/pr_discrim_tests/chuffed-maccDiscriminatingGen.sh
@@ -3,5 +3,5 @@
 echo $AUTOIG
 mkdir -p "$AUTOIG/experiments/macc-discrim-gen"
 cd "$AUTOIG/experiments/macc-discrim-gen"
-python3 "$AUTOIG/scripts/setup.py" --generatorModel "$AUTOIG/data/models/macc/generator.essence" --problemModel "$AUTOIG/data/models/macc/problem.mzn" --instanceSetting discriminating --minSolverTime 1 --maxSolverTime 10 --baseSolver chuffed --baseSolverFlags="-f" --favouredSolver picat --favouredSolverFlags="-f" --maxEvaluations 180 --genSolverTimeLimit 300
+python3 "$AUTOIG/scripts/setup.py" --generatorModel "$AUTOIG/data/models/macc/generator.essence" --problemModel "$AUTOIG/data/models/macc/problem.mzn" --instanceSetting discriminating --minSolverTime 1 --maxSolverTime 5 --baseSolver chuffed --baseSolverFlags="-f" --favouredSolver picat --favouredSolverFlags="-f" --maxEvaluations 180 --genSolverTimeLimit 3
 bash "$AUTOIG/experiments/macc-discrim-gen/run.sh"
