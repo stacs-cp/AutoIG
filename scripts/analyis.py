f5dfd69b7d2646752138e4b8e050d60215caaa21
TinTin <tw74@st-andrews.ac.uk>
2026-01-06 11:32:25 +0000

    bugfix

--- scripts/analyis.py
+++ scripts/analyis.py
@@ -35,6 +35,11 @@ def main():
         type=int
     )
 
+    parser.add_argument(
+        "--instanceSetting",
+        choices=["graded", "discriminating"],
+        default="graded"
+    )
 
 
     # read all settings into one variable and check setting validity
@@ -45,45 +50,81 @@ def main():
         lsLines = []
         okTimes = []
         okInst = []
-        for s in f.readlines():
-            data = json.loads(s)
-            if args.repair:
-                if "genunsat" in data["status"] or "gensolverTimeOut" in data["status"]:
-                    lsLines.append(data["status"])
-                else:
-
-                    data["instanceResults"]["results"]["main"]["runs"] = sorted(
-                        data["instanceResults"]["results"]["main"]["runs"], key=lambda run: run["solverTime"]
-                    )
-                    nRuns = len(data["instanceResults"]["results"]["main"]["runs"])
-                    medianRun = data["instanceResults"]["results"]["main"]["runs"][int(nRuns / 2)]
-
-                    if (medianRun["solverTime"] <= args.minTime):
-                        lsLines.append("tooEasy")
-                    elif (medianRun["solverTime"] >= args.maxTime):
-                        lsLines.append("tooDifficult")
+        print(args.instanceSetting)
+        if args.instanceSetting == "graded":
+            for s in f.readlines():
+                data = json.loads(s)
+                if args.repair:
+                    if "genunsat" in data["status"] or "gensolverTimeOut" in data["status"]:
+                        lsLines.append(data["status"])
                     else:
-                        lsLines.append("ok " + medianRun["status"])
+
+                        data["instanceResults"]["results"]["main"]["runs"] = sorted(
+                            data["instanceResults"]["results"]["main"]["runs"], key=lambda run: run["solverTime"]
+                        )
+                        nRuns = len(data["instanceResults"]["results"]["main"]["runs"])
+                        medianRun = data["instanceResults"]["results"]["main"]["runs"][int(nRuns / 2)]
+
+                        if (medianRun["solverTime"] <= args.minTime):
+                            lsLines.append("tooEasy")
+                        elif (medianRun["solverTime"] >= args.maxTime):
+                            lsLines.append("tooDifficult")
+                        else:
+                            lsLines.append("ok " + medianRun["status"])
+                            okTimes.append(medianRun["solverTime"])
+                            okInst.append(data["instanceResults"]["instance"])
+
+                else:
+                    if "ok" in data["status"]:
+                        nRuns = len(data["instanceResults"]["results"]["main"]["runs"])
+                        medianRun = data["instanceResults"]["results"]["main"]["runs"][int(nRuns / 2)]
+                        if "unsat" in medianRun["status"]:
+                            lsLines.append("ok unsat")
+                            # print(s)
+                        else:
+                            lsLines.append("ok sat")
                         okTimes.append(medianRun["solverTime"])
                         okInst.append(data["instanceResults"]["instance"])
-
-            else:
-                if "ok" in data["status"]:
-                    nRuns = len(data["instanceResults"]["results"]["main"]["runs"])
-                    medianRun = data["instanceResults"]["results"]["main"]["runs"][int(nRuns / 2)]
-                    if "unsat" in medianRun["status"]:
-                        lsLines.append("ok unsat")
-                        # print(s)
                     else:
-                        lsLines.append("ok sat")
-                    okTimes.append(medianRun["solverTime"])
-                    okInst.append(data["instanceResults"]["instance"])
+                        lsLines.append(data["status"])
+        elif args.instanceSetting == "discriminating":
+             for s in f.readlines():
+                data = json.loads(s)
+                print()
+                if args.repair:
+                    if "genunsat" in data["status"] or "gensolverTimeOut" in data["status"]:
+                        lsLines.append(data["status"])
+                    else:
+
+                        data["instanceResults"]["results"]["main"]["runs"] = sorted(
+                            data["instanceResults"]["results"]["main"]["runs"], key=lambda run: run["solverTime"]
+                        )
+                        nRuns = len(data["instanceResults"]["results"]["main"]["runs"])
+                        medianRun = data["instanceResults"]["results"]["main"]["runs"][int(nRuns / 2)]
+
+                        if (medianRun["solverTime"] <= args.minTime):
+                            lsLines.append("tooEasy")
+                        elif (medianRun["solverTime"] >= args.maxTime):
+                            lsLines.append("tooDifficult")
+                        else:
+                            lsLines.append("ok " + medianRun["status"])
+                            okTimes.append(medianRun["solverTime"])
+                            okInst.append(data["instanceResults"]["instance"])
+
                 else:
-                    lsLines.append(data["status"])
+                    if "ok" in data["status"]:
+                        score = float(data["instanceResults"]["score"])
+                        if score < -1:
+                            lsLines.append("favouredSolverWins")
+                        else:
+                            lsLines.append("baseSolverWins")
+                        
+                    else:
+                        lsLines.append(data["status"])
 
             
-        plt.savefig(os.path.join(os.path.dirname(args.fileName), "plot.png"))
-        plt.hist(okInst, okTimes)
+        # plt.savefig(os.path.join(os.path.dirname(args.fileName), "plot.png"))
+        # plt.hist(okInst, okTimes)
 
     print(Counter(lsLines).items())
 
