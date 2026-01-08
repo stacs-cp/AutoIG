0f8497f04d4c499ff365539b62b6f335b602aa49
TinTin <tw74@st-andrews.ac.uk>
2026-01-08 16:39:13 +0000

    Fix bug of never calling favoured solver,

--- scripts/evaluators/essence_discriminating.py
+++ scripts/evaluators/essence_discriminating.py
@@ -108,17 +108,19 @@ def evaluate_essence_instance_discriminating(
     for solverType in ["favoured", "base"]:
         solved = False
 
-        if solverType == "favouredSolver":
+        if solverType == "favoured":
             solverSetting = favouredSolverFlags
             current_solver  = favouredSolver
             if favouredSolver not in conf.solverInfo:
                 callSolver = favouredSolverCallSolver
 
-        else:
+        elif solverType == "base":
             solverSetting = baseSolverFlags
             current_solver  = baseSolver
             if baseSolver not in conf.solverInfo:
                 callSolver = baseSolverCallSolver
+        else:
+            Exception("This should never happen")
 
         # solverSetting = str(solver) + "Flags"
         print("Solversetting: ", solverSetting)
@@ -224,7 +226,7 @@ def evaluate_essence_instance_discriminating(
         if r["status"] not in ["sat", "unsat"]:
             solvedByAllBaseRuns = False
             break
-    print(solvedByAllBaseRuns)
+    print(f"Solved by all base runs: {solvedByAllBaseRuns}")
     if solvedByAllBaseRuns and (baseAvgTime < baseMinTime):
         print("\nInstance is too easy for the base solver. Quitting...")
         score = conf.SCORE_BASE_TOO_EASY    # Setting the type appropriately
