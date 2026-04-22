6e2ab78a5846d356d1c2c95946700affb7896fbc
Tianchen Wu <tw74@login01.hpc.st-andrews.ac.uk>
2026-04-22 20:24:26 +0100

    updating to use newest version of irace

--- bin/install-irace.sh
+++ bin/install-irace.sh
@@ -1,5 +1,5 @@
 name="irace"
-version="3.4.1"
+version="4.4.1"
 
 echo ""
 echo "============= INSTALLING $name ==================="
@@ -13,40 +13,44 @@ if ! [ -x "$(command -v R)" ]; then
     exit 1
 fi
 
-url="https://cran.r-project.org/src/contrib/irace_3.4.1.tar.gz"
+url="https://cran.r-project.org/src/contrib/irace_4.4.1.tar.gz"
 
-pushd $BIN_DIR
-
-mkdir -p $name
-
-SOURCE_DIR="$name-source"
-mkdir -p $SOURCE_DIR
-
-pushd $SOURCE_DIR
-#wget $url
-#tar zxf $(ls *.gz)
-cp $BIN_DIR/irace-source.zip ./
-unzip irace-source.zip
-#wget https://cran.r-project.org/src/contrib/R6_2.5.1.tar.gz
-#tar zxf R6_2.5.1.tar.gz
-#wget https://cran.r-project.org/src/contrib/data.table_1.14.2.tar.gz
-#tar zxf data.table_1.14.2.tar.gz
-#R CMD INSTALL -l $BIN_DIR/ R6
-cp $BIN_DIR/R-packages.R ./
-OS=$(uname)
-if [ "$OS" == "Darwin" ]; then
-    sed -i "" "s#<BIN_DIR>#$BIN_DIR#g" R-packages.R
-elif [ "$OS" == "Linux" ]; then
-    sed -i "s#<BIN_DIR>#$BIN_DIR#g" R-packages.R
-else
-    echo "Cannot determine your OS, uname reports: ${OS}"
-    exit 1
-fi
-Rscript R-packages.R
+Rscript -e "install.packages(c('https://cran.r-project.org/src/contrib/R6_2.6.1.tar.gz', 'https://cran.r-project.org/src/contrib/data.table_1.18.2.1.tar.gz', 'https://cran.r-project.org/src/contrib/matrixStats_1.5.0.tar.gz', 'https://cran.r-project.org/src/contrib/spacefillr_0.4.0.tar.gz', 'https://cran.r-project.org/src/contrib/digest_0.6.39.tar.gz'), lib='$BIN_DIR', repos=NULL, type='source')"
 export R_LIBS=$BIN_DIR/:$R_LIBS
-R CMD INSTALL -l $BIN_DIR/ irace
-popd
+Rscript -e "install.packages('$url', type='source')"
 
-rm -rf $SOURCE_DIR
+# pushd $BIN_DIR
 
-popd
+# mkdir -p $name
+
+# SOURCE_DIR="$name-source"
+# mkdir -p $SOURCE_DIR
+
+# pushd $SOURCE_DIR
+# wget $url
+# tar zxf irace_4.4.1.tar.gz
+# # cp $BIN_DIR/irace-source.zip ./
+# # unzip irace-source.zip
+# #wget https://cran.r-project.org/src/contrib/R6_2.5.1.tar.gz
+# #tar zxf R6_2.5.1.tar.gz
+# #wget https://cran.r-project.org/src/contrib/data.table_1.14.2.tar.gz
+# #tar zxf data.table_1.14.2.tar.gz
+# #R CMD INSTALL -l $BIN_DIR/ R6
+# cp $BIN_DIR/R-packages.R ./
+# OS=$(uname)
+# if [ "$OS" == "Darwin" ]; then
+#     sed -i "" "s#<BIN_DIR>#$BIN_DIR#g" R-packages.R
+# elif [ "$OS" == "Linux" ]; then
+#     sed -i "s#<BIN_DIR>#$BIN_DIR#g" R-packages.R
+# else
+#     echo "Cannot determine your OS, uname reports: ${OS}"
+#     exit 1
+# fi
+# Rscript R-packages.R
+# export R_LIBS=$BIN_DIR/:$R_LIBS
+# R CMD INSTALL -l $BIN_DIR/ "irace_4.4.1.tar.gz"
+# popd
+
+# rm -rf $SOURCE_DIR
+
+# popd
